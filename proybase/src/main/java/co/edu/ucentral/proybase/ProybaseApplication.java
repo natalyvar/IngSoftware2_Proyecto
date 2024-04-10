76e7cf40d75e7c37aa5fd6ddb3ecd25680825e81
ACCP <acorchuelop@ucentral.edu.co>
2024-04-09 20:42:21 -0500

    Commit inicial

---
+++ proybase/src/main/java/co/edu/ucentral/proybase/ProybaseApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.proybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProybaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProybaseApplication.class, args);
+	}
+
+}
